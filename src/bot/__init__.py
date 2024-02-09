5950dbde2981f3534055ce522191ae4e4cddc02a
darkfos <153763364+darkfos@users.noreply.github.com>
2024-02-10 01:44:40 +0300

    Добавил обработку обычных сообщений, рефакторинг кода

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,6 +1,6 @@
 # Работа с ботом
 
-from .handlers import commands_router
+from .handlers import commands_router, state_router, message_router
 from .text import get_message_start, get_message_help
 from .utils import set_commands
 from .states import ReportUser
